89cd1971442730df2bec930f89bcaaa83b28544f
nnaemeka obioha <nnaemeka.obioha@gmail.com>
2019-07-20 21:20:52 +0100

    Updated ap with a collection-item and colection-preview components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+
 
 import Homepage from './pages/homepage/Homepage'
+import Shop from './pages/shop/shop'
+
 
 function App() {
   return (
-    <div>
-      <Homepage/>
+    <div className="app">
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route exact path='/shop' component={Shop} />
+      </Switch>
+
     </div>
   );
 }
